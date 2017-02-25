c302fe64866e937140cd5bb726f579cf2ea5e2d2
Pothi Kalimuthu <pothi@tinywp.com>
2017-02-26 01:53:33 +0530

    Remove jQuery migrate

--- assets/js/combine-js.bash
+++ assets/js/combine-js.bash
@@ -3,7 +3,6 @@
 # combines the following scripts
 cat \
     ../../../../../wp-includes/js/jquery/jquery.js \
-    ../../../../..//wp-includes/js/jquery/jquery-migrate.min.js \
     > main.js
 
 echo 'var twentyseventeenScreenReaderText = {"quote":"<svg class=\"icon icon-quote-right\" aria-hidden=\"true\" role=\"img\"> <use href=\"#icon-quote-right\" xlink:href=\"#icon-quote-right\"><\/use> <\/svg>","expand":"Expand child menu","collapse":"Collapse child menu","icon":"<svg class=\"icon icon-angle-down\" aria-hidden=\"true\" role=\"img\"> <use href=\"#icon-angle-down\" xlink:href=\"#icon-angle-down\"><\/use> <span class=\"svg-fallback icon-angle-down\"><\/span><\/svg>"};' >> main.js
