78a22f7ddb878f7921c0ed5e66989d42ddcb1056
Pothi Kalimuthu <pothi@tinywp.com>
2017-02-27 11:38:43 +0530

    Split weights in webfont

--- assets/js/ffo-custom.js
+++ assets/js/ffo-custom.js
@@ -1,7 +1,32 @@
-var tinyfontA = new FontFaceObserver("Libre Franklin");
+var librelight = new FontFaceObserver("Libre Franklin", {
+    weight: 300,
+});
 
-tinyfontA.load().then(function() {
+var libre = new FontFaceObserver("Libre Franklin", {
+    weight: 400,
+});
+
+var libreitalic = new FontFaceObserver("Libre Franklin", {
+    weight: 400,
+    style: 'italic',
+});
+
+var librebold = new FontFaceObserver("Libre Franklin", {
+    weight: 600,
+});
+
+var libreextrabold = new FontFaceObserver("Libre Franklin", {
+    weight: 800,
+});
+
+Promise.all([
+        librelight.load(),
+        libre.load(),
+        libreitalic.load(),
+        librebold.load(),
+        libreextrabold.load()
+    ]).then(function () {
     document.documentElement.className += " webfonts-loaded";
-}, function() {
+}, function () {
     console.log('Web Font is not available after waiting 3 seconds');
 });
